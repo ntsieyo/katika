20c565ca256b735b5686cce679e32cb1011331e1
ntsieyo <ryde_w@yahoo.ca>
2023-05-27 12:13:53 +0200

    Removed admin config from transcribe.models and move  them to transcribe.admin This change allow to easily  find and configure the admin related stuff.

--- transcribe/admin.py
+++ transcribe/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from transcribe.models import Transcript
+
 # Register your models here.
+
+#class TranscriptAdmin(admin.ModelAdmin):
+#    pass
+
+
+#admin.site.register(Transcript, TranscriptAdmin)
+admin.site.register(Transcript)
