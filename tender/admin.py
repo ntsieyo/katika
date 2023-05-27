3bb073113ee76902146dbfc28675253c7afb7ea0
ntsieyo <ryde_w@yahoo.ca>
2023-05-27 10:33:51 +0200

    Removed admin config from tender.models and move  them to tender.admin This change allow to easily find the  admin related stuff.

--- tender/admin.py
+++ tender/admin.py
@@ -1,3 +1,97 @@
 from django.contrib import admin
 
+from tender.models import CDI_CRI, ArmpContract, ArmpEntry, Entreprise, EntrepriseChange, Exercice, TenderOwner, WBContract, WBProject, WBSupplier
+
 # Register your models here.
+
+class TenderOwnerAdmin(admin.ModelAdmin):
+
+    list_display = ('owner_id', 'short_name', 'full_name')
+    search_fields = ('owner_id', 'short_name', 'full_name')
+
+
+
+
+class ArmpEntryAdmin(admin.ModelAdmin):
+
+    list_display = ('owner', 'publication_datetime', 'cost', 'publication_type', 'title', 'link')
+    search_fields = ('title', 'content')
+    #search_fields = ('search_vector',)
+    list_filter = ('owner', 'publication_datetime', 'publication_type')
+    #filter_horizontal = ('supervisors', 'committee')
+    #raw_id_fields = ('author',)
+
+
+class CDI_CRIAdmin(admin.ModelAdmin):
+    list_display = ("cdi", "cri", "matches")
+    list_filter = ("cri",)
+    search_fields = ("matches",)
+
+
+
+class ExerciceAdmin(admin.ModelAdmin):
+
+    list_display = ('year', 'month')
+    search_fields = ('year', 'month')
+    list_filter = ('year', 'month')
+
+
+
+
+class EntrepriseAdmin(admin.ModelAdmin):
+
+    list_display = ('niu', 'raison_sociale','sigle', 'regime', 'forme_juridique', 'ville', 'telephone')
+    search_fields = ('raison_sociale', 'sigle', 'niu', 'telephone')
+    list_filter = ('regime', 'forme_juridique', 'ville', 'departement', 'region', 'etat_niu')
+
+
+class ArmpContractAdmin(admin.ModelAdmin):
+
+    list_display = ('maitre_ouvrage', 'status','reference', 'title', 'date', 'year', 'cost', 'titulaire')
+    search_fields = ('title', 'maitre_ouvrage', 'titulaire', 'reference')
+    list_filter = ('status', 'maitre_ouvrage', 'year')
+
+
+class WBProjectAdmin(admin.ModelAdmin):
+
+    list_display = ('project_id', 'start_date', 'cost', 'name')
+    search_fields = ('project_id', 'name', 'abstract', 'search_vector')
+    list_filter = ('financial_type', 'status', 'agency')
+    #exclude = ['search_vector']
+
+
+
+class WBSupplierAdmin(admin.ModelAdmin):
+
+    list_display = ('supplier_id', 'name')
+    search_fields = ('supplier_id', 'name', 'search_vector')
+    exclude = ['search_vector']
+
+
+
+
+class WBContractAdmin(admin.ModelAdmin):
+
+    list_display = ('date', 'get_project_id', 'cost', 'description')
+    list_filter = ('project__project_id',)
+    search_fields = ('search_vector',)
+
+
+
+
+
+admin.site.register(TenderOwner, TenderOwnerAdmin)
+
+admin.site.register(WBContract, WBContractAdmin)
+admin.site.register(WBSupplier, WBSupplierAdmin)
+
+admin.site.register(WBProject, WBProjectAdmin)
+
+admin.site.register(ArmpContract, ArmpContractAdmin)
+admin.site.register(ArmpEntry, ArmpEntryAdmin)
+
+admin.site.register(Entreprise, EntrepriseAdmin)
+admin.site.register(EntrepriseChange)
+
+admin.site.register(Exercice, ExerciceAdmin)
+admin.site.register(CDI_CRI, CDI_CRIAdmin)
--- tender/models.py
+++ tender/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib import admin
 import logging
 from django.contrib.postgres.search import SearchVectorField, SearchVector
 from django.contrib.postgres.indexes import GinIndex
@@ -94,27 +93,6 @@ class ArmpEntry(models.Model):
             self.save(update_fields=['search_vector'])
 
 
-class TenderOwnerAdmin(admin.ModelAdmin):
-
-    list_display = ('owner_id', 'short_name', 'full_name')
-    search_fields = ('owner_id', 'short_name', 'full_name')
-
-
-admin.site.register(TenderOwner, TenderOwnerAdmin)
-
-
-class ArmpEntryAdmin(admin.ModelAdmin):
-
-    list_display = ('owner', 'publication_datetime', 'cost', 'publication_type', 'title', 'link')
-    search_fields = ('title', 'content')
-    #search_fields = ('search_vector',)
-    list_filter = ('owner', 'publication_datetime', 'publication_type')
-    #filter_horizontal = ('supervisors', 'committee')
-    #raw_id_fields = ('author',)
-
-
-admin.site.register(ArmpEntry, ArmpEntryAdmin)
-
 
 class TenderSerializer(serializers.ModelSerializer):
 
@@ -163,13 +141,7 @@ class CDI_CRI(models.Model):
         verbose_name_plural = "CDIs"
 
 
-class CDI_CRIAdmin(admin.ModelAdmin):
-    list_display = ("cdi", "cri", "matches")
-    list_filter = ("cri",)
-    search_fields = ("matches",)
 
-
-admin.site.register(CDI_CRI, CDI_CRIAdmin)
 
 
 class CDI_CRI_Serializer(serializers.ModelSerializer):
@@ -240,14 +212,7 @@ class Exercice(models.Model):
         ordering = ["-year", "-month"]
 
 
-class ExerciceAdmin(admin.ModelAdmin):
 
-    list_display = ('year', 'month')
-    search_fields = ('year', 'month')
-    list_filter = ('year', 'month')
-
-
-admin.site.register(Exercice, ExerciceAdmin)
 
 
 class EntrepriseChange(models.Model):
@@ -283,7 +248,7 @@ class EntrepriseChange(models.Model):
             self.save(update_fields=['search_vector'])
 
 
-admin.site.register(EntrepriseChange)
+
 
 
 class Entreprise(models.Model):
@@ -430,15 +395,6 @@ class Entreprise(models.Model):
         ordering = ["sigle", "raison_sociale"]
 
 
-class EntrepriseAdmin(admin.ModelAdmin):
-
-    list_display = ('niu', 'raison_sociale','sigle', 'regime', 'forme_juridique', 'ville', 'telephone')
-    search_fields = ('raison_sociale', 'sigle', 'niu', 'telephone')
-    list_filter = ('regime', 'forme_juridique', 'ville', 'departement', 'region', 'etat_niu')
-
-
-admin.site.register(Entreprise, EntrepriseAdmin)
-
 
 class EntrepriseSerializer(serializers.ModelSerializer):
 
@@ -540,14 +496,7 @@ class ArmpContract(ContribuableMixin):
             self.save(update_fields=['search_vector'])
 
 
-class ArmpContractAdmin(admin.ModelAdmin):
 
-    list_display = ('maitre_ouvrage', 'status','reference', 'title', 'date', 'year', 'cost', 'titulaire')
-    search_fields = ('title', 'maitre_ouvrage', 'titulaire', 'reference')
-    list_filter = ('status', 'maitre_ouvrage', 'year')
-
-
-admin.site.register(ArmpContract, ArmpContractAdmin)
 
 ####################################################################################################
 ## WORLD BANK PROJECT
@@ -625,15 +574,6 @@ class WBProject(models.Model):
             self.save(update_fields=['search_vector'])
 
 
-class WBProjectAdmin(admin.ModelAdmin):
-
-    list_display = ('project_id', 'start_date', 'cost', 'name')
-    search_fields = ('project_id', 'name', 'abstract', 'search_vector')
-    list_filter = ('financial_type', 'status', 'agency')
-    #exclude = ['search_vector']
-
-
-admin.site.register(WBProject, WBProjectAdmin)
 
 
 class WBSupplier(models.Model):
@@ -669,14 +609,7 @@ class WBSupplier(models.Model):
             self.save(update_fields=['search_vector'])
 
 
-class WBSupplierAdmin(admin.ModelAdmin):
 
-    list_display = ('supplier_id', 'name')
-    search_fields = ('supplier_id', 'name', 'search_vector')
-    exclude = ['search_vector']
-
-
-admin.site.register(WBSupplier, WBSupplierAdmin)
 
 
 class WBContract(ContribuableMixin):
@@ -725,15 +658,6 @@ class WBContract(ContribuableMixin):
             #instance = self._meta.default_manager.with_documents().get(pk=self.pk)
             self.search_vector = SearchVector('description', 'team_fullname', config='french_unaccent')
             self.save(update_fields=['search_vector'])
-
-
-class WBContractAdmin(admin.ModelAdmin):
-
-    list_display = ('date', 'get_project_id', 'cost', 'description')
-    list_filter = ('project__project_id',)
-    search_fields = ('search_vector',)
-
-admin.site.register(WBContract, WBContractAdmin)
 
 
 
